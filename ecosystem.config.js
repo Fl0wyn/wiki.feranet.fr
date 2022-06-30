1b7dbaa031ee332f7c90ce3e8e63719974edb653
Fl0wyn <florian@feranet.fr>
2022-06-30 11:44:48 +0200

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+
+module.exports = {
+    apps: [
+        {
+            name: 'wiki-feranet',
+            port: process.env.PORT,
+            script: './node_modules/nuxt/bin/nuxt.js',
+            exec_mode: 'cluster_mode',
+            args: 'start'
+        }
+    ],
+    deploy: {
+        production: {
+            "host": process.env.HOST,
+            "path": process.env.PATH,
+            "ref": "origin/master",
+            "repo": "https://github.com/Fl0wyn/wiki.feranet.fr.git",
+            "post-deploy": "npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production"
+        }
+    }
+}
